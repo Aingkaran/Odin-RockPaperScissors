552bf3b08fbc3462b4cbdb6d908cb0d484db00fa
Aingkaran <aingkaran1995@gmail.com>
2022-06-14 22:00:28 -0400

    create function computerPlay()

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello world")
+function computerPlay(){
+    let randomnumb = Math.floor(Math.random() * 9);
+    console.log(randomnumb)
+    if (randomnumb<=2){
+        console.log("Rock")
+    }
+        else if (randomnumb<=5){
+            console.log("Paper")
+        }
+        else {
+            console.log("Scissors")
+        }
+    
+}
